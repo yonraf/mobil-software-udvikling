929b97fffc6b94e71376f19334a44adf255824f0
yonus <moraf19@student.sdu.dk>
2021-10-15 11:15:24 +0200

    Topbar center and fixes

--- justrandom/lib/screens/components/top_bar.dart
+++ justrandom/lib/screens/components/top_bar.dart
@@ -12,22 +12,19 @@ class TopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      padding: EdgeInsets.symmetric(vertical : MediaQuery.of(context).size.height * 0.06, horizontal: 5),
-      child: Row(
-        children: [
-          HomeButton(randomizer.themeColor),
-          Text(
-            randomizer.name,
-            style: TextStyle(
-              fontFamily: 'Abel',
-              color: Colors.black,
-              fontWeight: FontWeight.w200,
-              decoration: TextDecoration.none,
-              fontSize: 40,
-            ),
-          ),
-        ],
+      padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+      child: AppBar(
+        title: Text(randomizer.name, style: TextStyle(
+          fontFamily: 'Abel',
+          color: Colors.black,
+          fontWeight: FontWeight.w200,
+          decoration: TextDecoration.none,
+          fontSize: 40,
+        ),),
+        leading : HomeButton(randomizer.themeColor),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
       ),
     );
   }
