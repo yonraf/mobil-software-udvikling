ba8191f91c19177050b3f737bd4266e02ee3c590
Yusaf Tareen <gayssuck123@gmail.com>
2021-10-15 14:46:41 +0200

    Changed from stateless to stateful widget Dice Throw animations implemented

--- justrandom/lib/main.dart
+++ justrandom/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:justrandom/screens/randomizers/dice.dart';
 import 'package:justrandom/screens/randomizers/home.dart';
 
 
--- justrandom/lib/screens/randomizers/dice.dart
+++ justrandom/lib/screens/randomizers/dice.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:justrandom/constants.dart';
@@ -5,8 +7,15 @@ import 'package:justrandom/constants.dart';
 import '../components/top_bar.dart';
 import '../randomizer.dart';
 
-class Dice extends StatelessWidget implements Randomizer {
+class Dice extends StatefulWidget {
+  Dice();
 
+  @override
+  _DiceState createState() => _DiceState();
+  
+}
+
+class _DiceState extends State<Dice> implements Randomizer{
   @override
   String description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
@@ -16,13 +25,46 @@ class Dice extends StatelessWidget implements Randomizer {
   @override
   Color themeColor = kDiceThemeColor;
 
+  var imageArray = ["one.png", "two.png", "three.png", "four.png", "five.png", "six.png"];
+  int randomIntForDiceOne = Random().nextInt(6);
+  int randomIntForDiceTwo = Random().nextInt(6);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: kBackgroundColor,
       child: Column(
         children: [
-          TopBar(Dice())
+          TopBar(Dice()),
+          Text(
+            "You Rolled: " + (randomIntForDiceOne + randomIntForDiceTwo + 2).toString(),
+            style: const TextStyle(
+                fontFamily: 'Abel',
+                color: Colors.black,
+                fontWeight: FontWeight.w200,
+                decoration: TextDecoration.none,
+                fontSize: 28
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 25, 0, 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset("assets/"+imageArray[randomIntForDiceOne], height: 150, width: 150,),
+                Image.asset("assets/"+imageArray[randomIntForDiceTwo], height: 150, width: 150,)
+              ],
+            ),
+          ),
+          ElevatedButton(onPressed: run,
+            child: const Text("Roll Dice"),
+          )
+          /*Padding(
+            padding: const EdgeInsets.only(top: 60),
+            child: ElevatedButton(onPressed: changeImage,
+                child: const Text("Roll Dice"),
+            ),
+          )*/
         ],
       ),
     );
@@ -30,6 +72,10 @@ class Dice extends StatelessWidget implements Randomizer {
 
   @override
   void run() {
-    // TODO: implement run
+    setState(() {
+      randomIntForDiceOne = Random().nextInt(6);
+      randomIntForDiceTwo = Random().nextInt(6);
+    });
   }
+  
 }
